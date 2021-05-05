c611540d4c43d82a83efc7df18988ab1c73f648b
Culley Wilson <cul314@live.unc.edu>
2021-05-05 10:01:40 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+//import movieDB from './MovieDB.js';
+import SearchBar from './Search.js';
 
 function App() {
+
+
+  //console.log(mdb.getMovie(550));
+  //console.log(mdb.getMovieProviders(550));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar/>
     </div>
   );
 }
---
+++ src/MovieDB.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+export default class Movies {
+    constructor() {
+        this.base_url = 'https://api.themoviedb.org/3/';
+        this.image_url = null;
+        this.poster_size = 'w500';
+        this.api_key = 'd363ec5b2388fdeedd9b1c0a068b5fe3';
+
+        let url = this.base_url + 'configuration?api_key=' + this.api_key;
+        axios({
+            method: 'get',
+            url: url,
+        })
+            .then( res => {
+                this.image_url = res.data.images.secure_base_url;
+                console.log(res.data);
+            });
+    }
+}
+
+Movies.prototype.getMovie = async function(id) {
+    const result = await axios({
+        method: 'get',
+        url: `https://api.themoviedb.org/3/movie/${id}?api_key=${this.api_key}`,
+    });
+    return result;
+}
+
+Movies.prototype.getMovieProviders = async function(id) {
+    const result = await axios({
+        method: 'get',
+        url: `https://api.themoviedb.org/3/movie/${id}/watch/providers?api_key=${this.api_key}`,
+    });
+    return result;
+}
+
+Movies.prototype.getProviderCountries = async function() {
+    const result = await axios({
+        method: 'get',
+        url: `https://api.themoviedb.org/3/watch/providers/regions?api_key=${this.api_key}&language=en-US`,
+    });
+    return result;
+}
+
+Movies.prototype.searchMovie = async function(keyword) {
+    const result = await axios({
+        method: 'get',
+        url: `https://api.themoviedb.org/3/search/movie?api_key=${this.api_key}&query=${keyword}&page=1&include_adult=false`
+    })
+    return result;
+}
